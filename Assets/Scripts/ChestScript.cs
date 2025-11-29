b55acf7017a0aa6fead318810351835910a30be2
Beth Fisher <bethanyfisher2007.bf@gmail.com>
2025-11-29 17:49:56 +0000

    Fixed Chest Spawning Chests can now be spawned.

--- Assets/Scripts/ChestScript.cs
+++ Assets/Scripts/ChestScript.cs
@@ -3,23 +3,24 @@ using UnityEngine;
 public class ChestScript : MonoBehaviour
 {
     private int playerGold;
+    public GameObject chestPrefab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        SpawnChest();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //SpawnChest();
     }
 
     public void SpawnChest()
     {
         Vector2 newChestPosition = new(Random.Range(-10, 10), Random.Range(-10, 10));
-        Instantiate(Resources.Load("Prefabs/Chest"), newChestPosition, Quaternion.identity, transform);
+        Instantiate(chestPrefab, newChestPosition, Quaternion.identity, transform);
     }
 
     public void UpdateGold(int gold)
