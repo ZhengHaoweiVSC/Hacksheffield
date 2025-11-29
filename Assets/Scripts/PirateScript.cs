c948a1304c35920faf9c38fdc347ee8f91a709ee
Beth Fisher <bethanyfisher2007.bf@gmail.com>
2025-11-29 15:58:48 +0000

    Pirate Spawning System Pirates can now be spawned using a function in the PirateScript Class

--- Assets/Scripts/PirateScript.cs
+++ Assets/Scripts/PirateScript.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting; 
 using UnityEngine;
 
 public class PirateScript : MonoBehaviour
@@ -8,21 +9,45 @@ public class PirateScript : MonoBehaviour
     public void Start()
     {
         playerObject = GameObject.FindGameObjectWithTag("Player");
+
     }
 
     public void Update()
     {
-        // 
+
     }
 
-    public void spawnPirate()
+    public void SpawnPirate()
     {
         playerObject.GetComponent<PlayerScript>().getPlayerPosition();
 
         // Generate a position for the enemy to spawn at
-        Vector2 newPiratePosition = new(Random.Range(-10, 10), Random.Range(-10, 10));
+        Vector2 newPiratePosition = GenerateStartPosition();
+        Vector3 positionTranslation = new(newPiratePosition.x, newPiratePosition.y, 0);
 
+        // Spawn the pirate
+        Instantiate(piratePrefab, positionTranslation, new(0,0,0,0), gameObject.transform);
     }
+
+
+    private Vector2 GenerateStartPosition()
+    {
+        Vector2 playerPosition = playerObject.transform.position;
+
+        float randomRadius = Random.Range(150, 300) / 10f;
+        float bearing = Random.Range(0, 360);
+
+        // Find the components of the triangle formed when generating a random radius
+        // and random angle to offset the new pirate object by
+        float xOffset = (bearing <= 180) ? (randomRadius * Mathf.Cos(bearing % 90)) : -(randomRadius * Mathf.Cos(bearing % 90));
+        float yOffset = (bearing <= 90 || bearing >= 270) ? (randomRadius * Mathf.Sin(bearing % 90)) : -(randomRadius * Mathf.Sin(bearing % 90));
+
+        Vector2 spawnPosition = new Vector2(playerPosition.x + xOffset, playerPosition.y + yOffset);
+
+        return spawnPosition;
+    }
+
+
 
 
 }
