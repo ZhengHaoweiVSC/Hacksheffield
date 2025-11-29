00c1bb058b66dad90fb1ebd2d0fe0600221aca39
Beth Fisher <bethanyfisher2007.bf@gmail.com>
2025-11-29 14:46:11 +0000

    Pirate Update Added a pirate controller, Script and pirate specific class script. Not finished

--- Assets/Scripts/PirateScript.cs
+++ Assets/Scripts/PirateScript.cs
@@ -1,10 +1,28 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PirateScript : MonoBehaviour
 {
-    public static void Update()
+    GameObject playerObject;
+    public GameObject piratePrefab;
+
+    public void Start()
     {
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void Update()
+    {
+        // 
+    }
+
+    public void spawnPirate()
+    {
+        playerObject.GetComponent<PlayerScript>().getPlayerPosition();
+
+        // Generate a position for the enemy to spawn at
+        Vector2 newPiratePosition = new(Random.Range(-10, 10), Random.Range(-10, 10));
 
     }
+
+
 }
