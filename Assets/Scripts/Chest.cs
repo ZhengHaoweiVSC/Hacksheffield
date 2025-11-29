4ec41e2a0b2ff327761d5db0cd16fe651e60ee32
R-Con <atzhardwick1@sheffield.ac.uk>
2025-11-29 16:21:30 +0000

    Gold system

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -3,15 +3,59 @@ using UnityEngine;
 public class Chest : MonoBehaviour
 {
     private Vector2 chestPosition;
+    private ChestScript chestScript;
     private int goldAmount;
+    private float timeToLive;
+    private bool isCollected;
+
+    void Start()
+    {
+        chestScript = GameObject.FindGameObjectWithTag("ChestScript").GetComponent<ChestScript>();
+    }
 
     public Chest()
     {
         goldAmount = Random.Range(1, 10);
+        timeToLive = Time.time + 120.0f;
+        isCollected = false;
     }
 
     public Vector2 GetChestPosition()
     {
-        return chestPosition;
+        return gameObject.transform.position;
+    }
+
+    public int GetGoldAmount()
+    {
+        return goldAmount;
+    }
+    public bool IsCollected()
+    {
+        return isCollected;
+    }
+    public void CollectChest()
+    {
+        isCollected = true;
+    }
+
+    public float GetDistanceToPlayer()
+    {
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+        return Vector2.Distance(GetChestPosition(), playerObject.transform.position);
+    }
+
+    void Update()
+    {
+        if (Time.time > timeToLive)
+        {
+            Destroy(gameObject);
+        }
+
+        if (GetDistanceToPlayer() < 1 && !isCollected)
+        {
+            CollectChest();
+            isCollected = true;
+            chestScript.UpdateGold(goldAmount);
+        }
     }
 }
--- Assets/Scripts/ChestScript.cs
+++ Assets/Scripts/ChestScript.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class ChestScript : MonoBehaviour
 {
+    private int playerGold;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        SpawnChest();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void SpawnChest()
+    {
+        Vector2 newChestPosition = new(Random.Range(-10, 10), Random.Range(-10, 10));
+        Instantiate(Resources.Load("Prefabs/Chest"), newChestPosition, Quaternion.identity, transform);
+    }
+
+    public void UpdateGold(int gold)
+    {
+        playerGold = playerGold + gold;
     }
 }
